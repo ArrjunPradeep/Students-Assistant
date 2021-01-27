58831b2ac89769f19de8eeb30f9487e9fa36ac2d
Ubuntu <ubuntu@ip-172-31-17-9.us-east-2.compute.internal>
2021-01-27 14:30:46 +0000

    decodeImage_api_updated

--- app/routes/index.js
+++ app/routes/index.js
@@ -18,8 +18,8 @@ router.post("/decodeImage", async (req, res, next) => {
     const url = "https://api.mathpix.com/v3/text";
 
     // I N I T I A L I Z I N G    T H E    F I L E N A M E 
-    let image = req.files.image
-
+    //let image = req.files.image
+     let imageurl = req.body.imageUrl
     // M O V E    I M G    T O    K Y C    P A T H
     // id.mv(path.join(config.imgPath, id.name));
 
@@ -30,20 +30,20 @@ router.post("/decodeImage", async (req, res, next) => {
     headers["app_key"] = config.APP_KEY;
 
     // B U F F E R    O F    I M A G E    F I L E 
-    let dataBuffer = image.data
+    //let dataBuffer = image.data
 
     // M I M E 
-    var mime = image.mimetype
-    var imgType = mime.split(/[/]/);
+    //var mime = image.mimetype
+    //var imgType = mime.split(/[/]/);
 
     // I M A G E    T Y P E 
-    let mediaType = imgType[1];
+    //let mediaType = imgType[1];
 
     // I M A G E    D A T A    U R I 
-    var dataURI = await imageDataURI.encode(dataBuffer, mediaType)
+    //var dataURI = await imageDataURI.encode(dataBuffer, mediaType)
 
     const body = {
-      "src": dataURI,
+      "src": imageurl,
       "formats": ["text", "data", "html", "latex_styled"],
       "data_options": {
         "include_asciimath": true,
@@ -74,7 +74,7 @@ router.post("/decodeImage", async (req, res, next) => {
     }
 
     const modifiedText = data.replace(/(\\\r\n|\n|\r|\\)/gm, '')
-
+    console.log("MODIFIED TEXT:",modifiedText)
     return res.send({
       message: modifiedText,
       result: true

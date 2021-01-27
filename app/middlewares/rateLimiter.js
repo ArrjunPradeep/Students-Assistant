b8f783d06f943e08a7fca08b6c731aabbd33a58c
Ubuntu <ubuntu@ip-172-31-17-9.us-east-2.compute.internal>
2021-01-27 14:30:03 +0000

    middleware_updated

--- app/middlewares/rateLimiter.js
+++ app/middlewares/rateLimiter.js
@@ -2,10 +2,10 @@ const rateLimit = require("express-rate-limit");
 
 const rateLimiter = rateLimit({
     windowMs: 24 * 60 * 60 * 1000, // 1 month in ms , n the Gregorian calendar, an average month has exactly 30.436875 days.
-    max: 2,
+    max: 200,
     message: {
         status: "Fail",
-        message: 'You have exceeded the 2 requests in 24 hrs limit!'
+        message: 'You have exceeded the 200 requests in 24 hrs limit!'
     },
     headers: true
 })
